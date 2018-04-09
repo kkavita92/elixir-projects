1fc0cfe93e3f8418a0ba31f9a40db8650ea2cdba
Kavita Kalaichelvan <kkavita92@gmail.com>
2018-04-09 12:12:42 +0100

    Wires all components together

--- genstage_example/lib/genstage_example/application.ex
+++ genstage_example/lib/genstage_example/application.ex
@@ -1,19 +1,15 @@
 defmodule GenstageExample.Application do
-  # See https://hexdocs.pm/elixir/Application.html
-  # for more information on OTP Applications
-  @moduledoc false
-
   use Application
 
   def start(_type, _args) do
-    # List all child processes to be supervised
+    import Supervisor.Spec, warn: false
+
     children = [
-      # Starts a worker by calling: GenstageExample.Worker.start_link(arg)
-      # {GenstageExample.Worker, arg},
+      worker(GenstageExample.Producer, [0]),
+      worker(GenstageExample.ProducerConsumer, []),
+      worker(GenstageExample.Consumer, [])
     ]
 
-    # See https://hexdocs.pm/elixir/Supervisor.html
-    # for other strategies and supported options
     opts = [strategy: :one_for_one, name: GenstageExample.Supervisor]
     Supervisor.start_link(children, opts)
   end
